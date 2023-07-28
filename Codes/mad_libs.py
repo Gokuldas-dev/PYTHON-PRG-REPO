236cd8207c5c8464846e6e194fc2873f2e6e1a53
Gokuldas Shenoy.K <74420066+Gokuldas-dev@users.noreply.github.com>
2023-07-28 15:24:45 +0530

    Created  mad_libs.py
    This is a mad libs project created in colab using pythion

---
+++ Codes/mad_libs.py
@@ -0,0 +1,10 @@
+adjective1 = input("Enter an adjective :")
+noun =input("Enter an noun :")
+adjective2 =input("Enter an adjective :")
+verb =input("Enter an verb :")
+adjective3 =input("Enter an adjective :")
+
+print(f"Today i went to a {adjective1} Zoo.")
+print(f"In an Exhibit ,I saw a {noun}")
+print(f"{noun} was {adjective2} and {verb}ing ")
+print(f"I was {adjective3} ")
