2f495d18a296a17b39ea382e0911ae74e25381c3
Gokuldas Shenoy.K <74420066+Gokuldas-dev@users.noreply.github.com>
2023-07-29 11:36:58 +0530

    Create hypo_triangle.py

---
+++ Codes/hypo_triangle.py
@@ -0,0 +1,7 @@
+#hypotunus of a rightangle triangle
+ #c =sqrt(a^2 +b^2)
+import math
+a = float(input("Enter the value of side A :"))
+b = float(input("Enter the value of side B :"))
+c = math.sqrt(math.pow(a,2) + math.pow(b,2))
+print(f"The Hypotinus of the triangle is : {round(c,2)}")
