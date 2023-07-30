ed3404a59943c45cc0245ee7a0aa9badfad70029
Gokuldas Shenoy K <gokuldasshenoy2@gmail.com>
2023-07-30 21:33:12 +0530

    Created weight_converter

---
+++ Codes/weight_converter.py
@@ -0,0 +1,14 @@
+weight = float(input("Enter Your Weight : "))
+unit = input("KIlograms or Pounds ? (K or L): ")
+
+if unit == "K":
+    weight = weight * 2.205
+    unit ="Lbs"
+elif unit  == "L":
+    weight = weight / 2.205
+    unit = "Kgs"
+else:
+    print(f"{unit} was not valid")
+
+print(f"Your Weight is :{weight} {unit}")   
+   
