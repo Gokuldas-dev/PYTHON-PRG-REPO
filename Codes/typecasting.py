448405dbe90a393524b2fc07760fea6d7e0ad18a
Gokuldas Shenoy K <gokuldasshenoy2@gmail.com>
2023-09-12 19:29:10 +0530

    this is basic type casting method

---
+++ Codes/typecasting.py
@@ -0,0 +1,9 @@
+name = input("what is your name ? :")
+age = int(input("How old are you ? :"))
+
+age += 1
+
+print(f"Hello {name}")
+print(f"You are {age} Years old")
+
+print("You are " + str(age) +" Years old")
