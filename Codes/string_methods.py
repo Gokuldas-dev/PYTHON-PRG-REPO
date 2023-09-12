b15014c311b0661d650be0de9f7e81a24f8fef55
Gokuldas Shenoy K <gokuldasshenoy2@gmail.com>
2023-09-12 16:00:34 +0530

    diff string methods

---
+++ Codes/string_methods.py
@@ -0,0 +1,15 @@
+name = "gokuldas"
+
+print("name")
+print(name)
+print(f"name is : {name}")
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.count("o"))
+print(name.replace("o","a"))
+print(name.find("k"))
+print(name.isdigit())
+print(name.isalpha())
+print(name*3)
+print(len(name))
